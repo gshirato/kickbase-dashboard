d8a580bac9463a1819bfb114c45d96324a2ac7b0
gshirato <g8ta18@gmail.com>
2025-02-17 22:31:50 +0100

    APIHandlerクラスの認証情報をtokenに変更し、ユーザー設定やリーグ情報を取得するメソッドを追加

--- src/kickbase_dashboard/handler.py
+++ src/kickbase_dashboard/handler.py
@@ -9,8 +9,7 @@ from kickbase_dashboard.base import APIHandlerBase
 
 @dataclass(frozen=True)
 class APIHandler(APIHandlerBase):
-    username: str
-    password: str
+    token: str
     max_trials: int = 3
     retry_seconds: float = 0.1
 
@@ -31,18 +30,157 @@ class APIHandler(APIHandlerBase):
             print(f"Max number of trials reached ({n_trial}).")
             return None
 
-    def retrieve_areas(self):
+    def retrieve_user_settings(self):
         return self.retrieve_data(
-            os.path.join(self.base_url, "areas"),
+            os.path.join(self.base_url, "user", "settings"), {}, self.headers
+        )
+
+    def retrieve_leagues(self):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", "selection"),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_overview(self, league_id: str):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", league_id, "overview"),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_market(self, league_id: str):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", league_id, "market"),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_ranking(self, league_id: str, day_number: str):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", league_id, "ranking"),
+            {"dayNumber": day_number},
+            self.headers,
+        )
+
+    def retrieve_user_teamcenter(self, league_id: str, user_id: str, day_number: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "users", user_id, "teamcenter"
+            ),
+            {"dayNumber": day_number},
+            self.headers,
+        )
+
+    def retrieve_user_profile(self, league_id: str, user_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "managers", user_id, "profile"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_user_squad(self, league_id: str, user_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "managers", user_id, "squad"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_user_performance(self, league_id: str, user_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "managers", user_id, "performance"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_user_transfers(self, league_id: str, user_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "managers", user_id, "transfer"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_player(self, league_id: str, player_id: str):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", league_id, "players", player_id),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_player_marketvalue(
+        self, league_id: str, player_id: str, timeframe: str
+    ):
+        """
+        valid value for timeframe is unknown
+
+        """
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url,
+                "leagues",
+                league_id,
+                "players",
+                player_id,
+                "marketvalue",
+                timeframe,
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_player_performance(self, league_id: str, player_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "players", player_id, "performance"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_player_transfers(self, league_id: str, player_id: str):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url, "leagues", league_id, "players", player_id, "transfers"
+            ),
+            {},
+            self.headers,
+        )
+
+    def retrieve_league_player_transfer_history(
+        self, league_id: str, player_id: str, start: str
+    ):
+        return self.retrieve_data(
+            os.path.join(
+                self.base_url,
+                "leagues",
+                league_id,
+                "players",
+                player_id,
+                "transferHistory",
+            ),
+            {"start": start},
+            self.headers,
+        )
+
+    def retrieve_scouted_players(self, league_id: str):
+        return self.retrieve_data(
+            os.path.join(self.base_url, "leagues", league_id, "scoutedplayers"),
             {},
             self.headers,
         )
 
     @property
     def headers(self):
-        auth = base64.b64encode(f"{self.username}:{self.password}".encode())
-        return {"Authorization": f"Basic {auth.decode()}"}
+        return {"Authorization": f"Bearer {self.token}", "accept": "application/json"}
 
     @property
     def base_url(self):
-        return "https://apirest.wyscout.com/v4"
+        return "https://api.kickbase.com/v4"
